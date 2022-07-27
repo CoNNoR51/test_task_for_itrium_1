81f5b84a594e8910fe0dcae9aed07122133c59e4
Danila <danila.matveev.2017@gmail.com>
2022-07-27 20:42:04 +0300

    upd of gitignore

--- src/main/scala/hello_world.scala
+++ src/main/scala/hello_world.scala
@@ -2,7 +2,7 @@ import scala.io.StdIn
 
 object hello_world {
 
-  def main (args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
     val name = StdIn.readLine("enter ur name: ")
 
