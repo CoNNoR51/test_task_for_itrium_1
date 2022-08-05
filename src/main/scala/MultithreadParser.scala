0ddbe30be99e393d78653c192266a8d222601b84
Danila <danila.matveev.2017@gmail.com>
2022-08-05 17:02:44 +0300

    parsing JsArray doesn't work with case class
    It can't work because of field "ns" that is Int

---
+++ src/main/scala/MultithreadParser.scala
@@ -0,0 +1,48 @@
+import play.api.libs.json._
+import scalaj.http._
+
+import java.net.URLEncoder
+import scala.collection.mutable
+import scala.language.postfixOps
+
+class MultithreadParser {
+
+  def get_array_of_links(start_name: String) = {
+
+    val response: HttpResponse[String] = Http("https://en.wikipedia.org/w/api.php?action=parse&page="
+      + URLEncoder.encode(name_normalizer(start_name), "UTF-8") + "&prop=links&format=json").asString
+
+    val string_response = response.body.replace("\"*\"", "\"link\"")
+
+    val modified_json_from_response: JsValue = Json.parse(string_response)
+
+    (modified_json_from_response \ "parse" \ "links").get.validate[Example].get
+  }
+
+  def name_normalizer(start_name: String): String = {
+
+    if ((start_name(0) == '"') && (start_name(start_name.length - 1) == '"')) {
+      start_name.slice(1, start_name.length - 1).replace(" ", "_")
+    }
+    else {
+      start_name.replace(" ", "_")
+    }
+  }
+}
+
+case class LinkMap(map: mutable.Map[String, Any])
+
+case class Example(list: List[LinkMap])
+
+object Example{
+  implicit val exampleReads: Reads[Example] = Json.reads[Example]
+  implicit val exampleWrites: OWrites[Example] = Json.writes[Example]
+  implicit val exampleFormat: OFormat[Example] = Json.format[Example]
+}
+
+object LinkMap {
+  implicit val linkMapReads: Reads[LinkMap] = Json.reads[LinkMap]
+  implicit val linkMapWrites: OWrites[LinkMap] = Json.writes[LinkMap]
+  implicit val linkMapFormat: OFormat[LinkMap] = Json.format[LinkMap]
+}
+
--- src/main/scala/WikiParser.scala
+++ src/main/scala/WikiParser.scala
@@ -48,7 +48,7 @@ class WikiParser {
         + URLEncoder.encode(name_normalizer(start_name), "UTF-8") + "&prop=links&format=json").asString
       val list_of_ns = List(-1, -2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 100, 101, 118, 119, 710, 711, 828, 829, 2300, 2301, 2302, 2303)
 
-      var string_response = response.body.replace("*", "link")
+      var string_response = response.body.replace("\"*\"", "\"link\"")
 
       for (index <- list_of_ns) {
         string_response = string_response.replace("\"ns\":" + index.toString + ",", "\"ns\":\"" + index.toString + "\",")
